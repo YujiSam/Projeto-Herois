68768139e71d7a45c6710dc263e44bc60c13cedc
Yuji <gustavo_osugi@outlook.com>
2024-10-29 09:38:46 -0300

    Finalizando o Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,18 @@
+import facebook from '../../assets/facebook.png'
+import instagram from '../../assets/instagram.png'
+import twitterx from '../../assets/twitterx.png'
+
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+        <footer className= 'bg-pink-900'>
+            <div className='flex justify-center gap-8 pt-16'>
+                <img src={facebook} alt="Facebook" id="image" />
+                <img src={instagram} alt="Facebook" id="image" />
+                <img src={twitterx} alt="Facebook" id="image" />
+            </div>
+            <h1 className='text-center text-white pt-5 pb-2'>@2024-direitos reservados</h1>
+        </footer>
+    </>
   )
 }
