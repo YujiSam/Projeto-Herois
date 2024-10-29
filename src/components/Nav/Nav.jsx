79f3c2436771d5cdf34dd720e05c75750674c9cc
Yuji <gustavo_osugi@outlook.com>
2024-10-29 09:24:32 -0300

    Adicionando as Outras Rotas e Finalizando o Nav

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+export default function Nav() {
+  return (
+    <>
+        <header className="flex bg-red-950 text-red-400 items-center uppercase justify-between">
+            <h1 className="text-5xl p-10">Herois</h1>
+            <nav className="flex gap-5 items-center mr-5 text-xl">
+                <Link to="/">Home</Link>
+                <Link to="/produtos">Produtos</Link>
+                <Link to="/login" className="bg-white text-red-950 rounded-2xl p-3 pr-7 pl-7">Login</Link>
+            </nav>
+        </header>
+    </>
+  )
+}
